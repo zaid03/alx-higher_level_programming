619ea18bab7283f3d2584a34d65fa9bd33d395a3
zaid03 <terguyzaid@gmail.com>
2023-10-05 15:24:52 -0700

    tasks

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add_0
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-result = add(a, b)
-
-print('{} + {} = {}".format(a, b, result))
-	
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))	
